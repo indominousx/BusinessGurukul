cf78010d56f01e366a8ac8a1473cac6c43cac0f3
indominousx <krishnagandhi020@gmail.com>
2025-06-17 12:25:59 +0530

    Initial commit: Business Gurukul website setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Navbar from "./components/navbar";
+import ServicesOverview from "./pages/ServicesOverview";
+import BusinessConsultingServices from "./components/BusinessConsultingServices";
+import TechnologiaclServices from "./components/technologiaclservices";
+import VideoAdServices from "./components/videotiles";
+import DigitalMarketingServices from "./components/digitalmarketingservices";
+import AllServices from "./pages/AllServices";
 import "./App.css";
 
 const App = () => {
@@ -11,6 +17,12 @@ const App = () => {
       <div className="mt-6">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/services" element={<ServicesOverview />} />
+          <Route path="/services/business-consulting" element={<BusinessConsultingServices />} />
+          <Route path="/services/tech-development" element={<TechnologiaclServices />} />
+          <Route path="/services/video-ad" element={<VideoAdServices />} />
+          <Route path="/services/digital-marketing" element={<DigitalMarketingServices />} />
+          <Route path="/all-services" element={<AllServices />} />
         </Routes>
       </div>
     </Router>
